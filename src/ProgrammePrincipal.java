f579e2147adfc23e1c4ec5d5254eaa1639f2b7ca
jcdominguez <jcd.pro@gmail.com>
2024-02-21 16:04:21 +0100

    exercice 3

--- src/ProgrammePrincipal.java
+++ src/ProgrammePrincipal.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+
 public class ProgrammePrincipal {
     public static void main(String[] args) {
+
+        ArrayList<Tigre> tigres = new ArrayList<>();
+
         Tigre tigrou = new Tigre();
         tigrou.setNom("Tigrou");
         tigrou.setCouleur("marron");
@@ -9,5 +14,18 @@ public class ProgrammePrincipal {
 
         tigrou.setAge(5);
         System.out.println(tigrou.getAge());
+
+
+        tigres.add(tigrou);
+
+        Tigre caramel = new Tigre();
+        caramel.setCouleur("marron");
+        caramel.setNom("Caramel");
+        caramel.setAge(6);
+        tigres.add(caramel);
+
+        for(Tigre tigre : tigres){
+            System.out.println(tigre.getNom());
+        }
     }
 }
